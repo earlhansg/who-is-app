0f1ce596802ff7c0279c6f209c4e2133ab22f914
Earl Hans <earlhansg@gmail.com>
2024-08-06 18:50:04 +0800

    setup project in MERN typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-500 w-[300px]">
+        Testings
+      </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello worldss!
+      </h1>
+    </>
+  )
+}
+
+export default App
